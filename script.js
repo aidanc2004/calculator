83cc102e994c168abc75e058acd151819efb5c63
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2022-12-20 00:45:12 -0400

    Add operations + and -

--- script.js
+++ script.js
@@ -1,17 +1,108 @@
 let buttons = document.getElementById("buttons");
 let output = document.getElementById("output");
 
-for (let i = 1; i < 10; i++) {
-    // create button, add number and add class
+let number1 = 0; // first number entered into the calculator
+let number2 = 0; // second number entered
+let operation = ""; // current operation being performed ("+", "-", etc)
+
+// add a button that inputs a number into the calculator
+function addNumber(number) {
+    // create button, add number, and add class
     let button = document.createElement("button");
-    button.textContent = i;
+    button.textContent = number;
     button.className = "calculator-button";
-    
-    // add an event listener to it
+
+    // add a click event to it
     button.addEventListener("click", () => {
-        output.textContent = button.textContent + output.textContent
+        // if the output is "_", then clear it
+        if (output.textContent == "_") {
+            output.textContent = "";
+        }
+
+        // add number to the output
+        output.textContent += number
     })
 
     // append it to buttons
     buttons.appendChild(button);
 }
+
+// add an operator to the calculator ("+", "-", etc)
+function addOperator(symbol, callback) {
+    // create button, add symbol and add class
+    let button = document.createElement("button");
+    button.textContent = symbol;
+    button.className = "calculator-button";
+
+    // add a click event to it to callback function
+    button.addEventListener("click", callback);
+
+    // append it to buttons
+    buttons.appendChild(button);
+}
+
+// sets number1 to the current calculator output
+function inputNumber1() {
+    number1 = Number.parseInt(output.textContent);
+    output.textContent = "_"; // change output to "_" (empty)
+}
+
+// add two numbers
+function add() {
+    inputNumber1();
+    operation = "+";
+}
+
+// subtract two numbers
+function minus() {
+    inputNumber1();
+    operation = "-";
+}
+
+// inputs number2 and then solves
+function equals() {
+    number2 = Number.parseInt(output.textContent);
+
+    if (operation == "+") {
+        output.textContent = number1 + number2;
+    }
+
+    if (operation == "-") {
+        output.textContent = number1 - number2;
+    }
+}
+
+// wipes the calculator
+function clear() {
+    number1 = 0;
+    number2 = 0;
+    operation = "";
+    output.textContent = "_";
+}
+
+// add all of the buttons to the calculator in order
+function addButtons() {
+    // 1-3
+    for (let i = 1; i <= 3; i++) {
+        addNumber(i);
+    }
+    // add plus button
+    addOperator("+", add);
+    // 4-6
+    for (let i = 4; i <= 6; i++) {
+        addNumber(i);
+    }
+    // add minus button
+    addOperator("-", minus)
+    // 7-9
+    for (let i = 7; i <= 9; i++) {
+        addNumber(i);
+    }
+    // add equals button
+    addOperator("=", equals)
+
+    // add clear button
+    addOperator("clr", clear);
+}
+
+addButtons();
